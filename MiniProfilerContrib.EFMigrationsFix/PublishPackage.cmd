cc59bb11073b3c9b2a9941a848bc4f3c44b1df7f
John Rummell <john@rummellweb.com>
2015-01-14 14:09:20 -0500

    Initial code commit

---
+++ MiniProfilerContrib.EFMigrationsFix/PublishPackage.cmd
@@ -0,0 +1,12 @@
+﻿@echo off
+
+set version=1.0.0
+set project=MiniProfilerContrib.EFMigrationsFix
+set nuspecFile=%project%.nuspec
+set projectFile=%project%.csproj
+
+echo %project%
+echo %version%
+
+..\.nuget\nuget pack %nuspecFile% -Version %version%
+..\.nuget\nuget push %project%.%version%.nupkg
